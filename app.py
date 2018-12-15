4e7deb36cf2a2d5bea478d767bfc75a1f95ba0ff
cfrichardson1 <crichardsonsjsu@gmail.com>
2018-12-14 20:41:24 -0800

    added module "six"

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pprint import pprint
+import six
 import tweepy
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
